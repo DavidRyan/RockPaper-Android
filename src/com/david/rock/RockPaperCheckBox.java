0bf95dd1e7768ebf2856e13e02fd605710344b76
David Ryan <dryan31@gmail.com>
2012-04-01 22:37:35 -0500

    Update src/com/david/rock/RockPaperCheckBox.java

--- src/com/david/rock/RockPaperCheckBox.java
+++ src/com/david/rock/RockPaperCheckBox.java
@@ -14,34 +14,7 @@ import android.util.*;
 
 public class RockPaperCheckBox extends CheckBox {
 
-        private Paint paint;
-        private final int MIN_WIDTH = 200;
-        private final int MIN_HEIGHT = 50;
-        final int DEFAULT_COLOR = Color.WHITE;
-
-        int _color;
-
-
-		public RockPaperCheckBox(Context context) {
-			super(context);
-		}
-
-        public RockPaperCheckBox(Context context, AttributeSet attrs){
-        super(context, attrs);
-        }
-
-        public RockPaperCheckBox(Context context, AttributeSet attrs, int defStyle){
-        super(context, attrs, defStyle);
-        }
-
-		protected void onDraw(Canvas canvas) {
-			paint = new Paint();
-			//paint.setColor(Color.GREEN);
-            canvas.drawRect(5, 5, 5, 5, paint); 
-           // canvas.drawColor(_color);
-            super.onDraw(canvas);
-			//canvas.drawText("Hello World", 5, 30, paint);
-        }
-
-	}
+        //not done yet... obviously
+        
+}
 
